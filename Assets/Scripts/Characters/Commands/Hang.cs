23a62b071bb7699e0de48b225c46ce7507a8a059
jcortesjara <josecortesjaramillo@gmail.com>
2012-11-28 16:55:06 -0500

    Cubo Nube

---
+++ Assets/Scripts/Characters/Commands/Hang.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Hang : Command {
+
+    public Hang(CloudCube receiver, Vector3 endPosition) : base(receiver, endPosition){
+        
+    }
+
+    public override void Execute()
+    {
+        ((CloudCube)Cube).Hang(EndPosition);
+        EndExecution();
+    }
+}
---
+++ Assets/Scripts/Characters/Cubes/Naturals/CloudCube.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CloudCube : Cube {
+	
+		private bool fix;
+		
+		
+	// Use this for initialization
+	void Start () {
+		base.Start();
+		fix = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public override Command[] Options {
+		get {
+			List<Command>options = new List<Command>();
+			if(!fix){
+				for(int i = 0; i < 4;i ++){
+					Vector3 direction=Vector3.zero;
+					switch (i){
+					case 0:
+						direction=Vector3.forward;
+						break;
+					case 1:
+						direction=Vector3.forward*-1f;
+						break;
+					case 2:
+						direction = Vector3.right;
+						break;
+					case 3:
+						direction = Vector3.right*-1f;
+						break;
+					}
+					
+					if(transform.position.y-2>=0){
+						Debug.Log("abajo");
+					}
+					if( transform.position.y-2 > 0 && !Level.Singleton.Entities.ContainsKey(new Vector3(transform.position.x+direction.x,transform.position.y-2,transform.position.z+direction.z))&& !Level.Singleton.Entities.ContainsKey(new Vector3(transform.position.x+direction.x,transform.position.y-1,transform.position.z+direction.z))){
+	            		options.Add( new Hang(this, GetNextPositionCloude(direction)));
+					}else{
+						
+	            		options.Add( new Move(this, GetNextPositionCloude(direction)));
+					}
+				}
+	            //options.Add( new Move(this, GetNextPositionCloude(Vector3.forward)));
+	            //options.Add( new Move(this, GetNextPositionCloude(Vector3.forward * -1.0f)));
+	            //options.Add( new Move(this, GetNextPositionCloude(Vector3.right)));
+	            //options.Add( new Move(this, GetNextPositionCloude(Vector3.right * -1.0f)));
+				
+			
+	           for (int i = 0; i < options.Count; i++ )
+	            {
+					
+	                if(options[i].EndPosition.y - transform.position.y > 1)
+	                {
+	                    options.RemoveAt(i);
+	                }
+	                else
+	                {
+	                    if (options[i].EndPosition.x >= Level.Dimension || options[i].EndPosition.x < 0 || options[i].EndPosition.z >= Level.Dimension || options[i].EndPosition.z < 0)
+	                    {
+	                        options[i] = new OutOfBounds(this, options[i].EndPosition);
+	                    }
+	                }
+	            }
+				
+            	return options.ToArray();
+			}
+			
+			
+			
+			return options.ToArray();
+        }
+		
+	}
+	
+	public  Vector3 GetNextPositionCloude (Vector3 direction)
+{
+        Vector3 nextPosition = transform.position + direction.normalized;
+        bool isFree = !Level.Singleton.Entities.ContainsKey(nextPosition);
+        if (!isFree)
+        {
+            while (!isFree) {
+                nextPosition.y += 1.0f;
+                isFree = !Level.Singleton.Entities.ContainsKey(nextPosition);
+            }
+		
+        }
+        else
+        {
+            
+                nextPosition.y -= 1.0f;
+                if(!Level.Singleton.Entities.ContainsKey(nextPosition))
+				{
+					if(nextPosition.y < 0){
+                    	nextPosition.y += 1;
+						
+                }
+						return nextPosition;
+					
+					
+				}
+            
+            nextPosition.y += 1.0f;
+        }
+
+        return nextPosition;
+	
+    }
+	
+	
+	
+	public void Hang(Vector3 nextPosition){
+		fix=true;
+		Level.Singleton.Entities.Remove(transform.position);
+        transform.position = nextPosition;
+        Level.Singleton.Entities.Add(transform.position, this);
+		
+	}
+}
