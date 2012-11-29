ae5052f983e04159d05eff60304e99c8006afccf
Daniel Gomez Rico <danielgomezrico@gmail.com>
2012-11-29 16:53:53 -0500

    BounceAnimation in CubeAnimations added

--- Assets/Scripts/Utilities/Animations/CubeAnimations.cs
+++ Assets/Scripts/Utilities/Animations/CubeAnimations.cs
@@ -8,8 +8,8 @@ public class CubeAnimations
 	public static void AnimateDuplication (GameObject myGameObject)
 	{
 		//AnimationHelper.AnimateJump2(gameObject,Vector3.down,nextPosition,0f,"EndExecution",null);
-		myGameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-		AnimationHelper.AnimateGrow(myGameObject, 0f, "EndExecution", myGameObject);
+		myGameObject.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
+		AnimationHelper.AnimateGrow (myGameObject, 0f, "EndExecution", myGameObject);
 		//AnimationHelper.AnimateGrow(myGameObject, 0f, "EndExecution", null);
 
 	}
@@ -18,8 +18,14 @@ public class CubeAnimations
 	
 	#region Generic Cube Animations
 	
-	public static void AnimateMove(GameObject gameObject, Vector3 down, Vector3 nextPosition){
-		AnimationHelper.AnimateJump2(gameObject, down,nextPosition,0f,"EndExecution",null);
+	public static void AnimateMove (GameObject gameObject, Vector3 down, Vector3 nextPosition)
+	{
+		AnimationHelper.AnimateJump2 (gameObject, down, nextPosition, 0f, "EndExecution", null);
+	}
+	
+	public static void AnimateBounce (GameObject gameObject, Vector3 down, Vector3[] nextPositions)
+	{
+		AnimationHelper.AnimateBounce (gameObject, down, nextPositions, 0f, "EndExecution", null);
 	}
 	
 	#endregion
