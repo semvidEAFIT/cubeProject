d59fb00acc616e5ed1335f81e55074b3e7548513
Daniel Gomez Rico <danielgomezrico@gmail.com>
2012-11-29 17:29:47 -0500

    Move Script edited

--- Assets/Scenes/Scripts/Characters/Commands/Move.cs
+++ Assets/Scenes/Scripts/Characters/Commands/Move.cs
@@ -9,14 +9,7 @@ public class Move : Command {
 
     public override void Execute()
     {
-        if (EndPosition.x >= Level.Dimension || EndPosition.x < 0 || EndPosition.z >= Level.Dimension || EndPosition.z < 0)
-        {
-            Cube.FallOutOfBounds();
-        }
-        else 
-        {
-            Cube.MoveTo(EndPosition);
-        }
+		Cube.MoveTo(EndPosition);
         EndExecution();
     }
 }
